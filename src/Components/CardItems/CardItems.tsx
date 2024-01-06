7a96f01d462b9c00ede8b02e4a125169fa0e1909
MehdiShu <mehdidevelop3@gmail.com>
2024-01-06 13:39:59 +0330

    final version v-1

--- src/Components/CardItems/CardItems.tsx
+++ src/Components/CardItems/CardItems.tsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react'
-import './CardItems.css'
 import {ShopContext} from '../../Context/ShopContext'
 import remove_icon from '../Assets/cart_cross_icon.png'
 
@@ -15,8 +14,9 @@ const CardItems = () => {
 
 
   return (
-    <div className='carditems'>
-            <div className="cartitems-format-main">
+    <div className='my-10 mx-10 md:my-24 md:mx-40 overflow-hidden'>
+      {/* cartitems-format-main */}
+            <div className="hidden md:grid md:grid-cols-6 items-center">
               <p>Products</p>
               <p>Title</p>
               <p>Price</p>
@@ -28,46 +28,46 @@ const CardItems = () => {
             {all_product.map((e)=>{
                 if(cartItems[e.id]>0)
                 {
-                  return <div>
-                  <div className="cartitems-format cartitems-format-main">
-                    <img src={e.image} alt="" className='carticon-product-icon' />
-                    <p>{e.name}</p>
+                  return <div key={e.id} className=''>
+                  <div className="flex flex-wrap gap-10 md:gap-0 mt-2 md:grid md:grid-cols-6 md:items-center">
+                    <img src={e.image} alt="" className='w-1/2  rounded'/>
+                    <p className='w-2 md:w-auto'>{e.name}</p>
                     <p>${e.new_price}</p>
                     <button className='cartitems-quantity'>{cartItems[e.id]}</button>
                     <p>${e.new_price*cartItems[e.id]}</p>
-                    <img className='cartitems-remove-icon' src={remove_icon} onClick={()=> {removeFromCart(e.id)}} alt="" />
+                    <img className='h-2 md:h-4 cursor-pointer ' src={remove_icon} onClick={()=> {removeFromCart(e.id)}} alt="" />
                   </div>
                   <hr />
                 </div> 
               }
               return null;
             })}
-            <div className="cartitems-down">
-              <div className="cartitems-total">
-                <h1>cart Totals</h1>
+            <div className="flex flex-col-reverse mt-8 md:flex-row">
+              <div className="flex flex-1 flex-col mr-48 gap-10">
+                <h1>Cart Totals</h1>
                 <div>
-                    <div className="cartitems-total-item">
+                    <div className="flex justify-between gap-16">
                       <p>Subtotal</p>
                       <p>${getTotalCartAmount()}</p>
                     </div>
                     <hr />
-                    <div className="cartitems-total-item">
+                    <div className="flex justify-between gap-16">
                       <p>Shipping Fee</p>
                       <p>Free</p>
                     </div>
                     <hr />
-                    <div className="cartitems-total-item">
+                    <div className="flex justify-between gap-16">
                       <h3>Total</h3>
                       <h3>${getTotalCartAmount()}</h3>
                     </div>
                 </div>
-                <button>PROCEED TO CHECKOUT</button>
+                <button className='bg-[#ff5a5a] text-[#fff] w-[16rem] h-[3.5rem] text-base font-semibold cursor-pointer outline-none border-0 rounded'>PROCEED TO CHECKOUT</button>
               </div>
-              <div className="cartitems-promocode">
-                <p>If you have a promo code, Enter it here</p>
-                <div className="cartitems-promobox">
-                  <input type="text" placeholder='promo code' />
-                  <button>Submit</button>
+              <div className="flex-1 text-base font-medium">
+                <p className='text-[#555]'>If you have a promo code, Enter it here</p>
+                <div>
+                  <input className='w-[31rem] mt-4 bg-[#eaeaea] h-12 rounded mb-2' type="text" placeholder='promo code' />
+                  <button className='w-40 h-14 bg-black text-white cursor-pointer text-base rounded'>Submit</button>
                 </div>
               </div>
             </div>
--- src/Pages/LoginSingup.tsx
+++ src/Pages/LoginSingup.tsx
@@ -1,18 +1,17 @@
-import './CSS/LoginSingup.css'
 
 const LoginSingup = () => {
   return (
-    <div className='loginsingup'>
-        <div className="loginsingup-container">
+    <div className='bg-[#fce3fe] flex flex-col items-center h-[50rem] pt-[6rem] overflow-hidden'>
+        <div className="bg-white w-1/2 p-8 rounded-lg ">
           <h1>Sing Up</h1>
-          <div className="loginsingup-fields">
-            <input type="text" placeholder='Your Name' />
-            <input type="Email" placeholder='Email Address' />
-            <input type="password" placeholder='Password' />
+          <div className="flex flex-col gap-8 mt-8 mb-8 ">
+            <input className='h-16 rounded-lg text-[#5c5c5c] text-base' type="text" placeholder='Your Name' />
+            <input className='h-16 rounded-lg text-[#5c5c5c] text-base' type="Email" placeholder='Email Address' />
+            <input className='h-16 rounded-lg text-[#5c5c5c] text-base' type="password" placeholder='Password' />
           </div>
-          <button>Continue</button>
-          <p className="loginsingup-login">Already have an account <span>Login here</span></p>
-          <div className='loginsingup-agree'>
+          <button className='bg-[#ff4141] w-1/2 h-16 text-white rounded-lg cursor-pointer'>Continue</button>
+          <p className="text-[#5c5c5c] text-base font-medium">Already have an account <span>Login here</span></p>
+          <div className='flex items-center text-[#5c5c5c] text-base font-medium gap-5'>
             <input type="checkbox" name='' id='' />
             <p>By continuing, i agree to the terms of use & privacy policy.</p>
           </div>
